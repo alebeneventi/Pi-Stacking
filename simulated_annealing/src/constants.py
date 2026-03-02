35979587d469deea61119c03ce6dcfbdf655a312
alebeneventi <alebene98@gmail.com>
2026-03-02 11:46:58 +0100

    Create constants.py

---
+++ simulated_annealing/src/constants.py
@@ -0,0 +1,15 @@
+# Description: Constants used in the project
+
+# Atomic radii
+ATOMIC_RADII = {
+    'H': 0.25,
+    'C': 0.70,
+    'N': 0.65,
+    'O': 0.60,
+    'F': 0.50,
+    'P': 1.00,
+    'S': 1.00,
+    'Cl': 1.00,
+    'Br': 1.15,
+    'I': 1.40
+}
